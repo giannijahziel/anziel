84238d7df011e58d7d50d918cdf75ff880fb28fe
giannijahziel <giannijahziel@gmail.com>
2015-05-01 15:12:57 -0500

    Testing Notices

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,10 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#notice_wrapper").fadeOut("slow", function() {
+            $(this).remove();
+        })
+    }, 4500 );
+});
