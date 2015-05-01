7f8d6ebfe3d8271ba42e749ffe575f919546aaee
giannijahziel <giannijahziel@gmail.com>
2015-05-01 16:39:54 -0500

    Added parish filter

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307171859) do
+ActiveRecord::Schema.define(version: 20150501202124) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150307171859) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "parish_id"
+  end
+
+  create_table "parishes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
